83b2cc8711e1b723f8a7dee974381f518299adef
Aine McGivern <49028231+amcgivern723@users.noreply.github.com>
2022-08-06 21:46:16 +0100

    Update index.php

--- webProgramming/structure/index.php
+++ webProgramming/structure/index.php
@@ -1,3 +1,24 @@
+<?php
+
+    include 'assets/include.php';
+    require 'assets/dbInfo.php';
+
+    $query = "SELECT id, first_name, last_name, pen_name FROM Authors ORDER BY first_name";
+    $resultObj = $connection->query($query);
+
+    if(count($_POST) > 0) {
+        if($_POST['email'] != "")
+        {
+            header("Location: final.php");
+        }
+        else
+        {
+            $emailError = "validation";
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -12,18 +33,20 @@
 			</h2>
 		</div>        
         <div id="Body">
-            <form method="get" action="final.php" >
+            <form method="post" action="index.php" >
                 <div>
                     <label>Favorite Author:</label>
                     <select name="author">
-                        <option></option>
+                        <?php while($row = $resultObj->fetch_assoc()) : ?>
+                            <option value="<?=$row['id']?>"><?=$row['first_name']?> <?=$row['last_name']?></option>
+                        <?php endwhile; ?>
                     </select>
                 </div>		
                 <div class="multiple">
                     <label>Favorite Century:</label>
-                    17th Century <input type="checkbox" name="century" value="17th">
-                    18th Century <input type="checkbox" name="century" value="18th"> 
-                    19th Century <input type="checkbox" name="century" value="19th"> 
+                    17th Century <input type="checkbox" name="century[]" value="17th">
+                    18th Century <input type="checkbox" name="century[]" value="18th"> 
+                    19th Century <input type="checkbox" name="century[]" value="19th"> 
                 </div>
                 <div>
                     <label>Comments:</label>
@@ -33,7 +56,7 @@
                     <label>Name:</label>
                     <input type="text" name="name" />
                 </div>
-                <div>
+                <div class="<?=$emailError?>">
                     <label>E-mail Address:</label>
                     <input type="text" name="email" />
                 </div>
